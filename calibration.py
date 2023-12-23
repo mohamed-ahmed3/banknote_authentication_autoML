fe5a88fcbbf8df5547dbb74068d30403aa9f7f0b
Mohamed Hassan <18P3206@eng.asu.edu.eg>
2023-12-23 23:11:49 +0200

    Changed the calibration choice to sigmoid

--- calibration.py
+++ calibration.py
@@ -2,6 +2,6 @@ from sklearn.calibration import CalibratedClassifierCV
 
 
 def calibrate(model):
-    calibrated_model = CalibratedClassifierCV(model, method='isotonic', cv='prefit')
+    calibrated_model = CalibratedClassifierCV(model, method='sigmoid', cv='prefit')
 
     return calibrated_model
